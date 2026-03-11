c5d75312b71287f1ff392104f472383e5539dfef
hendreo santos <hendreosantos1@outlook.com>
2026-03-10 22:09:35 -0300

    implementando mensagem de token invalido

--- pdv/src/main/java/org/springboot/pdv/security/JwtAuthFilter.java
+++ pdv/src/main/java/org/springboot/pdv/security/JwtAuthFilter.java
@@ -1,9 +1,14 @@
 package org.springboot.pdv.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springboot.pdv.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,23 +33,36 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String authToken = request.getHeader("Authorization");
-        if (authToken != null && authToken.startsWith("Bearer")) {
-            String token = authToken.split(" ")[1];
-            String username = jwtService.getUserName(token);
 
-            UserDetails user = customUserDetailService.loadUserByUsername(username);
+        try {
+            String authToken = request.getHeader("Authorization");
+            if (authToken != null && authToken.startsWith("Bearer")) {
+                String token = authToken.split(" ")[1];
+                String username = jwtService.getUserName(token);
 
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                UserDetails user = customUserDetailService.loadUserByUsername(username);
 
-            authentication.setDetails(new WebAuthenticationDetailsSource()
-            .buildDetails(request));
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                authentication.setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(request));
 
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (RuntimeException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(convertObjectToJson(new ResponseDTO("Token Invalido!")));
         }
 
-        filterChain.doFilter(request, response);
+    }
+
+    public String convertObjectToJson(ResponseDTO object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
